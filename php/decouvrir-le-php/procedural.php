b21551d27199db8c0c21480a8b2787294d72eb67
Parfait NGUELE <82092630+ParfaitNguele@users.noreply.github.com>
2021-04-09 14:15:36 +0100

    Create procedural.php

---
+++ php/decouvrir-le-php/procedural.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Stocker la valeur 7 dans une variable
+Stocker la valeur 9 dans une autre variable
+*/
+$v1 = 7;
+$v2 = 9;
+
+//Additionner les deux valeurs et stocker le résultat dans une variable
+
+$s = $v1 + $v2;
+
+//Puis afficher la somme
+
+echo $s."<br/>";
+
+//Soustraire les deux valeur et stocker le résultat dans une variable
+
+$d = $v2 - $v1;
+
+//Puis afficher la différence
+
+echo $d."<br/>";
+
+//Diviser les deux valeurs et stocker le résultat dans une variable
+
+$r = $v1 / $v2;
+
+//Puis afficher la différence
+
+echo $r."<br/>";
+ ?>
